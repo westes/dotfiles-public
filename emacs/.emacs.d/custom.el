85ebaa4072da7ed606992b4d0e358b93ec3bf431
Will Estes <westes575@gmail.com>
2014-01-17 12:33:45 -0500

    customized TeX-PDF-mode to t in emacs

--- emacs/.emacs.d/custom.el
+++ emacs/.emacs.d/custom.el
@@ -3,6 +3,7 @@
  ;; If you edit it by hand, you could mess it up, so be careful.
  ;; Your init file should contain only one such instance.
  ;; If there is more than one, they won't work right.
+ '(TeX-PDF-mode t)
  '(column-number-mode t)
  '(comment-multi-line t)
  '(compilation-ask-about-save nil)
