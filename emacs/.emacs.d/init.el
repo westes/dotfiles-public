b2a8ffc6e74077a0d9dedcc198d92a3f428ac248
Will Estes <westes575@gmail.com>
2015-08-06 14:26:38 -0400

    update emacs init

--- emacs/.emacs.d/init.el
+++ emacs/.emacs.d/init.el
@@ -5,11 +5,6 @@
 
 (add-to-list 'load-path "~/share/emacs/")
 
-(require `unfill)
-
-(require `puppet-ext)
-(add-to-list 'auto-mode-alist '("\\.pp$" . puppet-mode))
-
 (condition-case err
 (progn
 (autoload 'speechd-speak "speechd-speak" nil t)
@@ -17,21 +12,16 @@
 (speechd-speak-set-language "en-US")
 (speechd-set-rate 100 t)
 (speechd-set-punctuation-mode `all t)
-(speechd-set-volume 60 t)
+(speechd-set-volume 25 t)
 )
 (error
-   (message "Cannot load speechd-el %s" (cdr err))))
+   (message "Cannot load speechd-el %s" (cdr err))))(require `unfill)
 
-(condition-case err
-    (progn
-      (autoload 'uniquify "uniquify" nil t)
-      )
-  (error
-     (message "Cannot load uniquify %s" (cdr err))))
+(require `puppet-ext)
+(add-to-list 'auto-mode-alist '("\\.pp$" . puppet-mode))
 
 (add-hook 'comint-output-filter-functions 'comint-strip-ctrl-m) 
 
-(electric-indent-mode)
 (ffap-bindings)
 
 (set-fill-column 72)
@@ -46,5 +36,19 @@
      (global-set-key "\C-cc" 'org-capture)
      (global-set-key "\C-ca" 'org-agenda)
      (global-set-key "\C-cb" 'org-iswitchb)
+
+; cycle-spacing introduced in emacs 24.4.
+(global-set-key "\M-SPC" 'cycle-spacing)
+
+                                        ; the new indentation engine for shell mode plays havok with speechd-el
+(setq sh-use-smie nil)
+
+                                        ; turn off character composition
+(global-auto-composition-mode -1)
+
+ (add-to-list 'auto-mode-alist '("\\.ledger$" . ledger-mode))
+
+;; Mutt support.
+(setq auto-mode-alist (append '(("/tmp/mutt.*" . mail-mode)) auto-mode-alist))
 
 (server-start)
